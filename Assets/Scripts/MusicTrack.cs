bfdb6be79180d1b2ff6d9f221966be201710d4f7
Van-Nafis <71866274+Van-Nafis@users.noreply.github.com>
2025-03-19 05:52:49 +0700

    Mijil Update

--- Assets/Scripts/MusicTrack.cs
+++ Assets/Scripts/MusicTrack.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class MusicTrack : MonoBehaviour
@@ -10,9 +11,52 @@ public class MusicTrack : MonoBehaviour
     public Tracker[] guruGatra;
     public int counter = 0;
 
+    public Slider musicSlider;
+
+    private bool isDragging = false;
+
     private void Start()
     {
         instance = this;
+        theMusic.Stop();
+
+        musicSlider.minValue = 0;
+        musicSlider.maxValue = theMusic.clip.length;
+        musicSlider.value = 0;
+
+        // Tambahkan event listener ke slider
+        musicSlider.onValueChanged.AddListener(delegate { OnSliderChanged(); });
+    }
+
+    private void Update()
+    {
+        // Update slider hanya jika tidak sedang di-drag
+        if (!isDragging && theMusic.isPlaying)
+        {
+            musicSlider.value = theMusic.time;
+        }
+    }
+
+    // Ketika slider mulai di-drag
+    public void OnPointerDown()
+    {
+        isDragging = true;
+    }
+
+    // Ketika slider dilepas
+    public void OnPointerUp()
+    {
+        isDragging = false;
+        theMusic.time = musicSlider.value; // Atur waktu pemutaran sesuai slider
+    }
+
+    // Ketika nilai slider berubah
+    public void OnSliderChanged()
+    {
+        if (isDragging)
+        {
+            theMusic.time = musicSlider.value;
+        }
     }
 
     [System.Serializable]
